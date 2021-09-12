9b2f4a6ef180589f542d93541f93b0c32dafc2f2
omkargb <omkargb107@gmail.com>
2021-09-12 21:08:03 +0530

    [omkargb] Add : Calculating Wages for a Month implemented

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,17 @@
-//Calculate Daily Employee Wage
+//Calculating Wages for a Month assuming 20 Working Days
 const IS_PART_TIME = 1;      const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;   const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const WORKING_DAYS=20;
 let empHrs=0;
-let empCheck=Math.floor(Math.random() * 10) % 3;  //gives 0,1 or 2
 
-empHrs=GetEmpWorkingHours(empCheck);   //function call
+for(let day = 0; day < WORKING_DAYS; day++){
+let empCheck=Math.floor(Math.random() * 10) % 3;  //gives 0,1 or 2
+empHrs=empHrs + GetEmpWorkingHours(empCheck);   //function call
+}
+
 let empWage = empHrs * WAGE_PER_HOUR;   //calculate from returned value
-console.log("Employee Wage for a day : "+empWage);
+console.log("Total hours : "+empHrs+" | Employee Wage : "+empWage);
 
 function GetEmpWorkingHours(empCheck)
 {
