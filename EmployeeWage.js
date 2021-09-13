9f4d804fb6bdb4a7a5acffdfe3346227ca4e3cb5
omkargb <omkargb107@gmail.com>
2021-09-14 00:44:05 +0530

    [omkargb] Add : //Store the Daily Wage along with the Total Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,20 +1,27 @@
-//Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month
+//Store the Daily Wage along with the Total Wage
 const IS_PART_TIME = 1;      const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;   const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const MAX_WORKING_DAYS=20;
 const MAX_HRS_IN_MONTH=160;
 let totalEmpHrs=0, empWorkingDays=0;
-
+let dailyWageArray=new Array();         //UC6- store daily wage
+let empWage=0, dailyWage=0;
+//Calculate Wages till a condition
 while(totalEmpHrs<=MAX_HRS_IN_MONTH && empWorkingDays<MAX_WORKING_DAYS)
 {
     empWorkingDays++;
-    let empCheck=Math.floor(Math.random() * 10) % 3;  //gives 0,1 or 2
-    totalEmpHrs=totalEmpHrs + GetEmpWorkingHours(empCheck);   //function call
+    let empCheck = Math.floor(Math.random() * 10) % 3;  //gives 0,1 or 2
+    let workingHrs = GetEmpWorkingHours(empCheck);
+    totalEmpHrs = totalEmpHrs +  workingHrs ;  //function call
+    empWage = totalEmpHrs * WAGE_PER_HOUR;   //calculate from returned value
+    //for wage per day
+    dailyWage = workingHrs * WAGE_PER_HOUR;
+    dailyWageArray.push(dailyWage);
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;   //calculate from returned value
-console.log("Total working Days : "+empWorkingDays+" | Total working hours : "+totalEmpHrs+" | Employee Wage : "+empWage);
+console.log("\nTotal working Days : "+empWorkingDays+" | Total working hours : "+totalEmpHrs+" | Month's Total Employee Wage : "+empWage);
+console.log("Daily wage : "+dailyWageArray)
 
 function GetEmpWorkingHours(empCheck)
 {
